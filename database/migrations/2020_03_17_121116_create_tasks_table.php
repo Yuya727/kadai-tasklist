70e59c5ed1f7912e7ba7abf8813a9b4fd4b6df2b
yuya.yamanaka <yamanaka1729@outlook.jp>
2020-03-19 08:35:53 +0000

    modified-3

--- database/migrations/2020_03_17_121116_create_tasks_table.php
+++ database/migrations/2020_03_17_121116_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->String("content");
+            $table->string("content");
             $table->timestamps();
             
         });
