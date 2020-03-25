6ee7c1de871185f71000fce7d21722482863c8d5
yuya.yamanaka <yamanaka1729@outlook.jp>
2020-03-25 09:38:41 +0000

    to

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -88,10 +88,10 @@ class TasksController extends Controller
     public function edit($id)
     {
         $task = Task::find($id);
-
-        return view('tasks.edit', [
-            'task' => $task,
-        ]);
+         if (\Auth::id() === $task->user_id){
+            return view('tasks.edit', [
+                'task' => $task,
+            ]);}
     }
 
     /**
@@ -102,14 +102,14 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {   
+    {   if (\Auth::id() === $task->user_id){
          $this->validate($request, [
             'status' => 'required|max:10',
         ]);
         $task = Task::find($id);
         $task->status = $request->status;  
         $task->content = $request->content;
-        $task->save();
+        $task->save();}
 
         return redirect('/');
     }
